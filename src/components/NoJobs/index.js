d17f63b35008f5008d80b926bcdb12bf2296577e
rajesh_r <rajeshececbit@gmail.com>
2025-02-11 12:56:28 +0000

    Added components folder and updated existing files

---
+++ src/components/NoJobs/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const NoJobs = () => (
+  <div className="no-jobs-container">
+    <img
+      className="no-jobs-img"
+      src="https://assets.ccbp.in/frontend/react-js/no-jobs-img.png"
+    />
+    <h1>No Jobs Found</h1>
+    <p>We could not find any jobs. Try other filters.</p>
+  </div>
+)
+
+export default NoJobs
